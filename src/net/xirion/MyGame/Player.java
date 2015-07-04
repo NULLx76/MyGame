66f82dab7e0f6f57b2cb0fe7bb38137f7f9d20ac
victorheld <victorroest@hotmail.com>
2015-07-04 22:20:40 +0200

    Added New Enemy Changed Every int to float

--- src/net/xirion/MyGame/Player.java
+++ src/net/xirion/MyGame/Player.java
@@ -1,51 +1,37 @@
 package net.xirion.MyGame;
 
 import java.awt.*;
-
+import java.util.Random;
 public class Player extends GameObject{
-
+    Random r = new Random();
     Handler handler;
-
-    public Player(int x, int y, ID id, Handler handler) {
+    public Player(int x,int y, ID id, Handler handler){
         super(x, y, id);
         this.handler = handler;
-
     }
-
-    public void tick() {
-        x+= velX;
-        y+= velY;
-
-        x = Game.clamp(x, 0, Game.WIDTH - 37);
+    public Rectangle getBounds(){
+        return new Rectangle((int) x,(int) y, 32, 32);
+    }
+    public void tick(){
+        x += velX;
+        y += velY;
+        x = Game.clamp(x, 0, Game.WIDTH -37);
         y = Game.clamp(y, 0, Game.HEIGHT - 60);
-
-        handler.addObject(new Trail(x,y,ID.Trail,Color.BLUE,32,32,0.05f,handler));
-
+        handler.addObject(new Trail(x, y, ID.Trail, Color.white, 32, 32, 0.05f,handler));
         collision();
-
     }
-
     private void collision(){
-        for(int i =0; i < handler.object.size(); i++ ){
+        for(int i = 0; i < handler.object.size(); i++){
             GameObject tempObject = handler.object.get(i);
-
-            if(tempObject.getId() == ID.BasicEnemy || tempObject.getId() == ID.FastEnemy){
+            if(tempObject.getId() == ID.BasicEnemy || tempObject.getId()==ID.FastEnemy||tempObject.getId()== ID.SmartEnemy){
                 if(getBounds().intersects(tempObject.getBounds())){
-                    //collision code
                     HUD.HEALTH -= 2;
                 }
             }
         }
     }
-
-    public void render(Graphics g) {
-       g.setColor(Color.BLUE);
-        g.fillRect(x,y,32,32);
+    public void render(Graphics g){
+        g.setColor(Color.white);
+        g.fillRect((int)x,(int)y,32,32);
     }
-
-    public Rectangle getBounds() {
-        return new Rectangle(x,y,32,32);
-    }
-
-
 }
