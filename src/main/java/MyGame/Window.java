2f55e0133fc4640c6f6d3b3cb46799b434b0f0d8
victorheld <victorroest@hotmail.com>
2015-07-08 11:46:31 +0200

    Changed Window.java

--- src/main/java/MyGame/Window.java
+++ src/main/java/MyGame/Window.java
@@ -14,12 +14,12 @@ class Window extends Canvas {
         frame.setPreferredSize(new Dimension(width, height));
         frame.setMaximumSize(new Dimension(width, height));
         frame.setMinimumSize(new Dimension(width, height));
-
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.add(game);
         frame.setVisible(true);
+
         game.start();
     }
 }
